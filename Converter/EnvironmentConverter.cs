ea70f21518b81221346a0958ba6f322dad157b6e
BoWen <cbwwork@outlook.com>
2020-12-23 14:07:47 +0800

    可以运行的基于wpf core 的cefSharp 项目

---
+++ Converter/EnvironmentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CefSharpExampleNetCore.Converter
+{
+    public class EnvironmentConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Environment.Is64BitProcess ? "x64" : "x86";
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ Converter/TitleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CefSharpExampleNetCore.Converter
+{
+    public class TitleConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "CefSharp.MinimalExample.Wpf - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
