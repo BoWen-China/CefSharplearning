1a1c20c418b71228335f2c1eea7debff08d72108
BoWen <cbwwork@outlook.com>
2020-12-24 17:57:59 +0800

    加入flash插件以及处理方法

---
+++ Handler/MenuHandler.cs
@@ -0,0 +1,35 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CefSharpExampleNetCore.Handler
+{
+    class MenuHandler : IContextMenuHandler
+    {
+        // 在上下文功能菜单调用前
+        // IMenuModel 创建和修改菜单
+        public void OnBeforeContextMenu(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            // 清除菜单
+            model.Clear();
+        }
+
+        public bool OnContextMenuCommand(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            //throw new NotImplementedException();
+            return false;
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public bool RunContextMenu(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+        {
+            //throw new NotImplementedException();
+            return false;
+        }
+    }
+}
