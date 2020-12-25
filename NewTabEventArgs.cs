a3177d97b6212e157bef01ce2884082ab998cdd0
BoWen <cbwwork@outlook.com>
2020-12-25 17:51:48 +0800

    新标签处理类

---
+++ NewTabEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CefSharpExampleNetCore
+{
+    class NewTabEventArgs : EventArgs
+    {
+        public string Url { get; private set; }
+        public NewTabEventArgs(string url)
+        {
+            Url = url;
+        }
+
+    }
+}
