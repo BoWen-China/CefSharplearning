02451c15ba2fa735e61d530c08c88d5c009f324d
BoWen <cbwwork@outlook.com>
2020-12-25 17:50:40 +0800

    菜单处理类

--- Handler/MenuHandler.cs
+++ Handler/MenuHandler.cs
@@ -15,17 +15,23 @@ namespace CefSharpExampleNetCore.Handler
             model.Clear();
         }
 
+        // 调用该函数用来执行从上下文菜单中选择的命令
         public bool OnContextMenuCommand(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
         {
             //throw new NotImplementedException();
+            //false 默认执行
+            //true  命令被处理
             return false;
         }
-
+        // 当上下文菜单被取消时调用，不管菜单是否为空或命令是否被选择。
         public void OnContextMenuDismissed(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame)
         {
            // throw new NotImplementedException();
         }
 
+        // 调用来允许自定义显示上下文菜单。
+        // 自定义显示返回true 并于选定的命令ID同步或异步执行回调。
+        // 默认显示返回false
         public bool RunContextMenu(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
         {
             //throw new NotImplementedException();
