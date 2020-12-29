a5a616a6cf72a9b277543d0be2ae982c13be36b5
BoWen <cbwwork@outlook.com>
2020-12-29 17:57:21 +0800

    请求处理类

---
+++ Handler/TangRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using CefSharp.Handler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CefSharpExampleNetCore.Handler
+{
+    public class TangRequestHandler : RequestHandler
+    {
+    }
+}
