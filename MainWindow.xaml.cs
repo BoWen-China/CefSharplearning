a229045e8f4f704fc3f45b87e950d4f58d42f095
BoWen <cbwwork@outlook.com>
2021-01-08 17:42:28 +0800

    调整参数

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,6 +22,20 @@ namespace CefSharpExampleNetCore
         public event EventHandler OpenInNewtab = delegate { };
         public MainWindow()
         {
+            // 获取启动应用程序可执行文件的路径
+            string startUpPath = System.Windows.Forms.Application.StartupPath;
+            // 初始化设置
+            CefSettings cefSettings = new CefSettings();
+            cefSettings.Locale = "zh-Cn";
+            cefSettings.CefCommandLineArgs.Add("enable-system-flash", "1"); //启用flash
+            cefSettings.CefCommandLineArgs.Add("enable-media-stream", "1"); //启用媒体流
+            cefSettings.CefCommandLineArgs.Add("ppapi-flash-version", "26,0,0,126"); //设置flash插件版本
+            cefSettings.CefCommandLineArgs.Add("ppapi-flash-path", startUpPath + @"Plugins\pepflashplayer.dll");
+            // 要在浏览器对象初始化运行前使用
+            // 用用户提供的设置初始化CefSharp 
+            // initialize 和Shutdown 必须在你的主应用程序线程（通常UI 线程）调用
+            // 坚持
+            Cef.Initialize(cefSettings,true);
             InitializeComponent();
             CreateNewTab(DefaultUrl);
         }
@@ -53,19 +67,6 @@ namespace CefSharpExampleNetCore
                 }
                 e.Handled = true;
             };
-            // 获取启动应用程序可执行文件的路径
-            string startUpPath = System.Windows.Forms.Application.StartupPath;
-            // 初始化设置
-            CefSettings cefSettings = new CefSettings();
-            cefSettings.Locale = "zh-Cn";
-            cefSettings.CefCommandLineArgs.Add("enable-system-flash", "1"); //启用flash
-            cefSettings.CefCommandLineArgs.Add("enable-media-stream", "1"); //启用媒体流
-            cefSettings.CefCommandLineArgs.Add("ppapi-flash-version", "26,0,0,126"); //设置flash插件版本
-            cefSettings.CefCommandLineArgs.Add("ppapi-flash-path", startUpPath + @"Plugins\pepflashplayer.dll");
-            if(!Cef.IsInitialized)
-            {
-                Cef.Initialize(cefSettings);
-            }
         }
 
         // 开启新的标签页生命周期
